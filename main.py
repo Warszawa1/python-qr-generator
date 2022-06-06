bb91e5e46434c5f79c5590a6a5b9644a1d93d081
Irene Antelo <iantelovazquez@gmail.com>
2022-06-06 13:25:07 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import qrcode
+
+input_data = "ap"
+
+qr = qrcode.QRCode(
+    version=1,
+    box_size=8,
+    border=2)
+
+qr.add_data(input_data)
+qr.make(fit=True)
+
+img = qr.make_image(fill='black', back_color='white')
+img.save('qrcodeIA.png')
